9b67364976e74e0b1fe31860a7706f8c49a4fee1
Papali Priya Ratan Sahu <sahu.ppr@gmail.com>
2024-11-09 23:42:33 +0530

    added

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,27 @@
-import "@/styles/globals.css"
-import "@/styles/animation.css"
-import "@/styles/cssGrid.css"
-import "@/styles/Home.module.css"
-import { Analytics } from "@vercel/analytics/react"
+import "@/styles/globals.css";
+import "@/styles/animation.css";
+import "@/styles/cssGrid.css";
+import "@/styles/Home.module.css";
+import { Analytics } from "@vercel/analytics/react";
 import ReactGA from "react-ga4";
 
-import { PortfolioProvider } from "@/contextApi/PortfolioContext"
-import ThemeProvider from "@/context/themeContext"
+import { PortfolioProvider } from "@/contextApi/PortfolioContext";
+import ThemeProvider from "@/context/themeContext";
 
-ReactGA.initialize(process.env.NEXT_PUBLIC_MEASUREMENT_ID);
-
+// Initialize Google Analytics only if in production and the Measurement ID is provided
+if (process.env.NODE_ENV === "production" && process.env.NEXT_PUBLIC_MEASUREMENT_ID) {
+    ReactGA.initialize(process.env.NEXT_PUBLIC_MEASUREMENT_ID);
+} else if (!process.env.NEXT_PUBLIC_MEASUREMENT_ID) {
+    console.warn("Google Analytics Measurement ID is missing");
+}
 
 export default function App({ Component, pageProps }) {
-
-  return (
-    <PortfolioProvider>
-      <ThemeProvider>
-        <Component {...pageProps} />
-        <Analytics />
-      </ThemeProvider>
-    </PortfolioProvider>
-  )
+    return (
+        <PortfolioProvider>
+            <ThemeProvider>
+                <Component {...pageProps} />
+                <Analytics />
+            </ThemeProvider>
+        </PortfolioProvider>
+    );
 }
